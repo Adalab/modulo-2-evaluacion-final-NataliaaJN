e8c575b2ed2f019bcf9648a32e24aa64ef111c0e
Natalia <atvnataliajn@gmail.com>
2021-12-29 20:32:23 +0100

    update

--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,21 @@
 // Recoger los elementos de HTML
 const searchInput = document.querySelector('.js-searchInput');
 const searchBtn = document.querySelector('.js-searchBtn');
+
 const containerResults = document.querySelector('.container__result');
-const seriesSearchResults = document.querySelector('.js-seriesResult');
-const favoritesList = document.querySelector('.js-favoritesList');
+//const seriesSearchResults = document.querySelector('.js-seriesResult');
 //const resultsList = document.querySelector('.js-resultsList');
+
+const favoritesList = document.querySelector('.js-favoritesList');
+
 const urlApi = `https://api.jikan.moe/v3/search/anime?q=`;
+
+
 // variables globales
 let seriesResults = []; // array para los resultados
 let favourites = []; // array para los favoritos
+
+
 //               COGER DATOS DEL API           //
 const getApiData = (searchInputValue) => {
   return fetch(`${urlApi}${searchInputValue}`)
@@ -18,23 +25,27 @@ const getApiData = (searchInputValue) => {
     .then((data) => data.results);
   // .catch(error=> console.warn(error.message));
 };
+
+
 //        MOSTRAR RESULTADOS DE BÚSQUEDA            //
+
 // Función para generar HTML
 const getResultsHtmlCode = (eachResult) => {
   let resultHtmlCode = '';
   if (eachResult.image_url === null) {
-    resultHtmlCode += `  <li data-name= "${eachResult.title}" class= 'js-newLiElement'>
+    resultHtmlCode += `  <li id= "${eachResult.mal_id}" class= 'js-newLiElement'>
                           <img src='https://via.placeholder.com/210x295/ffffff/666666/?text=${eachResult.title}'; alt='${eachResult.title}'>
                           <h2>${eachResult.title}</h2>  
                         </li>`;
   } else {
-    resultHtmlCode += ` <li data-name= "${eachResult.title}" class= 'js-newLiElement'>
+    resultHtmlCode += ` <li id= "${eachResult.mal_id}" class= 'js-newLiElement'>
                         <img src='${eachResult.image_url}' alt='${eachResult.title}'>
                         <h2>${eachResult.title}</h2>  
                     </li>`;
   }
   return resultHtmlCode;
 };
+
 // Función para pintar los resultados
 const renderResults = (htmlElement, results) => {
   let resultsCode = '';
@@ -46,6 +57,8 @@ const renderResults = (htmlElement, results) => {
   // escucho eventos
   // listenGetSearch();
 };
+
+// Escuchar evento para pintar resultados
 searchBtn.addEventListener('click', function (event) {
   event.preventDefault();
   const searchTerm = searchInput.value;
@@ -66,69 +79,28 @@ searchBtn.addEventListener('click', function (event) {
   }
 });
 
-// Escuchar evento para pintar resultados
-// añadir a la lista de favoritos el elemento que coincide con el title clickado
+
+
+// Añadir a la lista de favoritos
+
 const addFavourite = (ev) => {
-  const selectSerieTitle = ev.currenTarget.dataset.title;
-  console.log(selectSerieTitle);
-  // buscar en los resultados el elemento que coincide con el title clickado
-  let foundFavourite = favourites.find(
-    (favourite) => favourite.title === selectSerieTitle
-  );
-  // si no lo he encontrado en la lista de favoritos
-  if (foundFavourite === undefined) {
-    let foundSerie = seriesResults.find(
-      (serie) => serie.title === selectSerieTitle
-    ); //busco la serie clickada en los resultados
-    // y lo añado a favoritos
-    favourites.push(foundSerie);
-    foundFavourite.classList.add('selectSerie');
-  } else {
-    //busco el índice del elemento clickado
-    const isFavourite = favourites.findIndex(
-      (favourite) => favourite.title === selectSerieTitle
-    );
-    favourites.splice(isFavourite, 1); //Lo borro de la lista de favoritos
+  console.log(ev.currentTarget);
+  const selectedSerie= parseInt(ev.currentTarget.id);
+
+
+};
+
+
+// Evento click en cada resultado mostrado
+const listenResults= () => {
+  let allResults= document.querySelectorAll('.js-newLiElement');
+  for (const resultEl of allResults){
+    resultEl.addEventListener('click', addFavourite);
   }
-  //setInLocalStorage();
-  renderResults();
-  //renderFavourites();// pintar los favoritos
 };
-// Escuchar evento click en resultados: 1º-crear clase para los resultados
-const listenAddResultsToFavourites = () => {
-  const liResults = document.querySelectorAll('.js-newLiElement');
-  // creo un listener para cada resultado
-  for (const eachResult of liResults) {
-    eachResult.addEventListener('click', addFavourite);
-  }
-  // addFavourite();
-};
-listenAddResultsToFavourites();
-//               HACER FAVORITOS            //
-const getFavouriteHtmlCode = (favourite) => {
-  let favouriteCode = '';
-  favouriteCode += `<li class= "selectSerie">
-                       <img src='${favourite.image_url}' alt='${favourite.title}'>
-                        <h2 data-title= "title">${favourite.title}</h2>  
-                    </li>
-    `;
-  return favouriteCode;
-};
-//     Guardar en localStorage     //
-const setInLocalStorage = () => {
-  // guardar los favoritos
-  const stringifyFavourites = JSON.stringify(favourites); // pasar el objeto a cadena de  texto
-  localStorage.setItem('favourites', stringifyFavourites);
-};
-// Pintar favoritos
-const renderFavourites = () => {
-  favoritesList.innerHTML = ''; // limpiamos
-  for (const favourite of favourites) {
-    favoritesList.innerHTML += getFavouriteHtmlCode(favourite);
-  }
-  // addFavourite();
-};
-// Listener para el botón de buscar
-// searchBtn.addEventListener('click', listenGetSearch);
-// start app
-renderFavourites();
+
+// Delego el listener en el padre (porque los elementos li no existen cuando carga la página)
+// containerResults.addEventListener('click', addFavourite);
+
+
+
